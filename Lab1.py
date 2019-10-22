22739abe60ce8c846ff8f15931977eae0ddefbb2
rulli-aji-gunawan <rully.okayama@gmail.com>
2019-10-22 17:55:40 +0700

    commit semua file

---
+++ Lab1.py
@@ -0,0 +1,22 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: F:\01. LOCAL DRIVE D\01. KAMPUS\Bahasa Pemrograman\Latihan Pemrograman Semester 1\Belajar Python Awal\Python_Lab1.Git\Python_Lab1.py
+ABCTraceback (most recent call last):
+  File "F:\01. LOCAL DRIVE D\01. KAMPUS\Bahasa Pemrograman\Latihan Pemrograman Semester 1\Belajar Python Awal\Python_Lab1.Git\Python_Lab1.py", line 5, in <module>
+    Print ()
+NameError: name 'Print' is not defined
+>>> 
+= RESTART: F:\01. LOCAL DRIVE D\01. KAMPUS\Bahasa Pemrograman\Latihan Pemrograman Semester 1\Belajar Python Awal\Python_Lab1.Git\Python_Lab1.py
+ABC
+XYZ
+>>> 
+>>> 
+= RESTART: F:\01. LOCAL DRIVE D\01. KAMPUS\Bahasa Pemrograman\Latihan Pemrograman Semester 1\Belajar Python Awal\Python_Lab1.Git\Python_Lab1.py
+ABC
+XYZ10 15 20 25
+10,15,20,25
+10152025
+10:15:20:25
+10-----15-----20-----25
+>>> 
